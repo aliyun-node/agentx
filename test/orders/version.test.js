921cf2be7744f5d24546c240a7103ce3261216d6
Jackson Tian <shyvo1987@gmail.com>
2015-10-09 17:32:28 +0800

    add version report

---
+++ test/orders/version.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var path = require('path');
+var expect = require('expect.js');
+var version = require('../../lib/orders/version');
+
+describe('/lib/orders/version.js', function () {
+  before(function () {
+    version.init({
+      cmddir: path.join(__dirname, '../cmddir')
+    });
+  });
+
+  it('should ok', function (done) {
+    version.run(function (err, params) {
+      expect(err).not.to.be.ok();
+      expect(params.type).to.be('version');
+      expect(params.metrics).to.be.ok();
+      var metrics = params.metrics;
+      expect(metrics).to.have.key('node');
+      done();
+    });
+  });
+});
