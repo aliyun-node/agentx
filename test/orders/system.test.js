a708469d7ab6dcdedb4908903bf972ad314a9c9f
Jackson Tian <shyvo1987@gmail.com>
2016-09-08 17:34:25 +0800

    report cpu count in system directive

--- test/orders/system.test.js
+++ test/orders/system.test.js
@@ -11,6 +11,11 @@ describe('/lib/orders/system.js', function () {
       expect(params.metrics).to.be.ok();
       var metrics = params.metrics;
       expect(metrics).to.have.key('uptime');
+      expect(metrics).to.have.key('load1');
+      expect(metrics).to.have.key('load5');
+      expect(metrics).to.have.key('load15');
+      expect(metrics).to.have.key('cpu');
+      expect(metrics).to.have.key('cpu_count');
       done();
     });
   });
