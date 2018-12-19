9ea6c31fd0f861c28c567a4bb7491f179af681c4
hyj1991 <hugh.hyj@alibaba-inc.com>
2018-12-19 11:14:53 +0800

    test: improve unit test coverage

--- test/orders/tracing_log.test.js
+++ test/orders/tracing_log.test.js
@@ -65,9 +65,52 @@ describe('/lib/orders/tracing_log.js', function () {
         expect(item.traceId).to.be('1e28345415263728868931001d0660');
       });
 
-      // restore log file
-      fs.writeFileSync(logPath, log);
-      done();
+      // append array
+      var append2 = '[{"traceId":"1e28345415263728868931001d0661","spanId":"1e28345415263728868931002d0660","parentSpanId":"1e28345415263728868921000d0660","operationName":"child_span1","startTime":1526372886893,"rootTime":1526372886892,"duration":8713,"logs":[{"startTimeFromFullTrace":1,"logMessage":{"state":"timer1"}}],"tags":[{"timeout":8708.907699750824}]},'
+        + '"dashboard $ cat /tmp/node-20151210.log",'
+        + '{"traceId":"1e28345415263728868931001d0661","spanId":"1e28345415263728868931003d0660","parentSpanId":"1e28345415263728868921000d0660","operationName":"child_span1","startTime":1526372886893,"rootTime":1526372886892,"duration":9751,"logs":[{"startTimeFromFullTrace":1,"logMessage":{"state":"timer1"}}],"tags":[{"timeout":9746.823575296385}]},'
+        + '{"traceId":"1e28345415263728868931001d0661","spanId":"1e28345415263728966441004d0660","parentSpanId":"1e28345415263728868921000d0660","operationName":"child_span2","startTime":1526372896644,"rootTime":1526372886892,"duration":3003,"logs":[{"startTimeFromFullTrace":9752,"logMessage":{"state":"timer2"}}],"tags":[{"timeout":"3s"}]},'
+        + '{"traceId":"1e28345415263728868931001d0661","spanId":"1e28345415263728996481005d0660","parentSpanId":"1e28345415263728868921000d0660","operationName":"child_span3","startTime":1526372899648,"rootTime":1526372886892,"duration":38,"logs":[{"startTimeFromFullTrace":12756,"logMessage":{"state":"rpc"}}],"tags":[{"http request":"http://47.100.164.242/we"}]},'
+        + '{"traceId":"1e28345415263728868931001d0661","spanId":"1e28345415263728868921000d0660","parentSpanId":null,"operationName":"root request","startTime":1526372886892,"rootTime":1526372886892,"duration":12797,"logs":[{"startTimeFromFullTrace":1,"logMessage":{"state":"start"}}],"tags":[]},'
+        + '"dashboard $ cat /tmp/node-20151210.log"]\n';
+
+      fs.appendFileSync(logPath, append2);
+
+      tracingLog.run(function (err, params) {
+        expect(err).not.to.be.ok();
+        expect(params.type).to.be('tracing_log');
+        expect(params.metrics).to.be.ok();
+        var metrics = params.metrics;
+        expect(metrics.ok).to.be.ok();
+        expect(metrics.data).to.have.length(5);
+        metrics.data.forEach(function (item) {
+          expect(item.traceId).to.be('1e28345415263728868931001d0661');
+        });
+
+        // crossover date
+        mm(helper, 'getYYYYMMDD', function () {
+          return '20180517';
+        });
+
+        tracingLog.run(function (err, params) {
+          expect(err).not.to.be.ok();
+          expect(params.type).to.be('tracing_log');
+          expect(params.metrics).to.be.ok();
+          var metrics = params.metrics;
+          expect(metrics.ok).to.be.ok();
+          expect(metrics.data).to.have.length(5);
+          metrics.data.forEach(function (item) {
+            expect(item.traceId).to.be('1e28345415263728868931001d0662');
+          });
+        });
+
+        // restore log file
+        fs.writeFileSync(logPath, log);
+        mm(helper, 'getYYYYMMDD', function () {
+          return '20180515';
+        });
+        done();
+      });
     });
   });
 
@@ -78,7 +121,7 @@ describe('/lib/orders/tracing_log.js', function () {
       expect(params.metrics).to.be.ok();
       var metrics = params.metrics;
       expect(metrics.ok).to.be.ok();
-      expect(metrics.data).to.have.length(0);
+      expect(metrics.data).to.have.length(5);
       done();
     });
   });
@@ -87,6 +130,30 @@ describe('/lib/orders/tracing_log.js', function () {
     var getCurrentLogPath = tracingLog.__get__('getCurrentLogPath');
     var logPath = path.join(__dirname, '../logdir/tracing-20180515.log');
     expect(getCurrentLogPath()).to.be(logPath);
+  });
+
+  describe('tracing log error', function () {
+    before(function () {
+      mm(helper, 'getYYYYMMDD', function () {
+        return '20180516';
+      });
+
+      tracingLog.init({
+        logdir: path.join(__dirname, '../logdir')
+      });
+    });
+
+    after(function () {
+      mm.restore();
+    });
+
+    it('should not ok', function (done) {
+      tracingLog.run(function (err) {
+        expect(err).to.be.ok();
+        expect(err.message.includes('is not a file')).to.be(true);
+        done();
+      });
+    });
   });
 
   describe('no logdir', function () {
@@ -118,3 +185,4 @@ describe('/lib/orders/tracing_log.js', function () {
     });
   });
 });
+
